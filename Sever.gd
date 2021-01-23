94f37e778ba6a162f6173131f841d9ea45cad06a
Jkazuk <jeffreykazuk@gmail.com>
2021-01-22 18:18:44 -0600

    Update Sever.gd

--- Sever.gd
+++ Sever.gd
@@ -7,25 +7,25 @@ var IP_SERVER = "0.0.0.0"
 var socketUDP = PacketPeerUDP.new()
 
 func _ready():
-    start_server()
+	start_server()
 
 func _process(delta):   
 
-    if socketUDP.get_available_packet_count() > 0:
-        var array_bytes = socketUDP.get_packet()
-        var IP_CLIENT = socketUDP.get_packet_ip()
-        var PORT_CLIENT = socketUDP.get_packet_port()
-        printt("msg server: " + array_bytes.get_string_from_ascii())
-        socketUDP.set_dest_address(IP_CLIENT, PORT_CLIENT)
-        var stg = "hi client!"
-        var pac = stg.to_ascii()
-        socketUDP.put_packet(pac)
+	if socketUDP.get_available_packet_count() > 0:
+		var array_bytes = socketUDP.get_packet()
+		var IP_CLIENT = socketUDP.get_packet_ip()
+		var PORT_CLIENT = socketUDP.get_packet_port()
+		printt("msg server: " + array_bytes.get_string_from_ascii())
+		socketUDP.set_dest_address(IP_CLIENT, PORT_CLIENT)
+		var stg = "hi client!"
+		var pac = stg.to_ascii()
+		socketUDP.put_packet(pac)
 
 func start_server():
-    if (socketUDP.listen(PORT_SERVER) != OK):
-        printt("Error listening on port: " + str(PORT_SERVER))
-    else:
-        printt("Listening on port: " + str(PORT_SERVER))
+	if (socketUDP.listen(PORT_SERVER) != OK):
+		printt("Error listening on port: " + str(PORT_SERVER))
+	else:
+		printt("Listening on port: " + str(PORT_SERVER))
 
 func _exit_tree():
-    socketUDP.close()
+	socketUDP.close()
